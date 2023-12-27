a55280b805878e95293d90ea397f9b389bdd0418
WahyuAji11 <alhafidzw1@gmail.com>
2023-12-27 20:19:20 +0800

    third commit

--- src/app/component/EmailSection.jsx
+++ src/app/component/EmailSection.jsx
@@ -40,16 +40,16 @@ const EmailSection = () => {
             <div className="bg-[radial-gradient(ellipse_at_center,_var(--tw-gradient-stops))] from-primary-900 to-transparent rounded-full h-80 w-80 z-0 blur-lg absolute top-3/4 -left-4 transform -translate-x-1/2 -translate-1/2">
             </div>
             <div>
-                <h5 className="text-xl font-bold text-white my-2">Let's Connect</h5>
+                <h5 className="text-xl font-bold text-white my-2">Let&apos;s Connect</h5>
                 <p className="text-[#ADB7BE] mb-4 max-w-md">currently looking for new opportunities, my inbox is always
         open. Whether you have a question or just want to say hi, I&apos;ll
         try my best to get back to you!</p>
                 <div className="socials flex flex-row gap-2">
                     <Link href="https://github.com/WahyuAji11" target="_blank" >
-                        <Image src={GithubIcon} alt="" />
+                        <Image src={GithubIcon} alt="github" />
                     </Link>
                     <Link href="https://www.linkedin.com/in/wahyu-aji-389681273/" target="_blank" >
-                        <Image src={LinkedInIcon} alt=""/>
+                        <Image src={LinkedInIcon} alt="linkedin"/>
                     </Link>
                 </div>
             </div>
--- src/app/component/Footer.jsx
+++ src/app/component/Footer.jsx
@@ -6,7 +6,7 @@ const Footer = () => {
     return (
     <footer className="footer border z-10 border-t-[#33353F] border-l-transparent border-r-transparent text-white">
         <div className="container p-12 flex justify-between align-items-center">
-            <span className="relative bottom-3.5"><Image src={Logoimg} className="w-14" alt="" /></span>
+            <span className="relative bottom-3.5"><Image src={Logoimg} className="w-14" alt="logo" /></span>
             <p className="text-slate-600">All rigths reserved.</p>
         </div>
     </footer>
